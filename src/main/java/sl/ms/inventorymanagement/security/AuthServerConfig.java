d3afe48342934b450d5d85c0742878319c15ca20
MANI <skmani2@gmail.com>
2021-01-02 22:03:53 +0530

    enable oauth

---
+++ src/main/java/sl/ms/inventorymanagement/security/AuthServerConfig.java
@@ -0,0 +1,63 @@
+package sl.ms.inventorymanagement.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+	private String clientid = "clientid";
+	//private String clientSecret = "mysecretkey";
+	private String clientSecret = "$2a$10$Viet8sOjNE4Hcwrx4uoYXeajQGRjcqhAimNItUGHJcqSY4nSBQoYG";
+	@Value("${auth.private}")
+	private String privateKey;
+	@Value("${auth.public}")
+	private String publicKey;
+
+	@Autowired
+	@Qualifier("authenticationManagerBean")
+	private AuthenticationManager authenticationManager;
+
+	@Bean
+	public JwtAccessTokenConverter tokenEnhancer() {
+		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		converter.setSigningKey(privateKey);
+		converter.setVerifierKey(publicKey);
+		return converter;
+	}
+
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(tokenEnhancer());
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore())
+				.accessTokenConverter(tokenEnhancer());
+	}
+
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient(clientid).secret(clientSecret).scopes("read", "write")
+				.authorizedGrantTypes("password", "refresh_token").accessTokenValiditySeconds(20000)
+				.refreshTokenValiditySeconds(20000);
+
+	}
+}
---
+++ src/main/java/sl/ms/inventorymanagement/security/CustomUser.java
@@ -0,0 +1,13 @@
+package sl.ms.inventorymanagement.security;
+
+import org.springframework.security.core.userdetails.User;
+
+import sl.ms.inventorymanagement.entity.UserEntity;
+
+public class CustomUser extends User {
+	private static final long serialVersionUID = 1L;
+
+	public CustomUser(UserEntity user) {
+		super(user.getUsername(), user.getPassword(), user.getGrantedAuthoritiesList());
+	}
+}
