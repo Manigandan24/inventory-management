4a643a25c78b636390779360cc02f7c19aacafea
Manigandan24 <skmani2@gmail.com>
2025-01-08 21:17:41 +0530

    add more logics for kafka

--- src/main/java/sl/ms/inventorymanagement/config/KafkaConsumer.java
+++ src/main/java/sl/ms/inventorymanagement/config/KafkaConsumer.java
@@ -6,7 +6,9 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.errors.ResourceNotFoundException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.ConsumerRecordRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 import org.springframework.util.backoff.BackOff;
 import org.springframework.util.backoff.FixedBackOff;
@@ -18,19 +20,20 @@ import sl.ms.inventorymanagement.exception.KafkaCustomErrorHandler;
 @Service
 public class KafkaConsumer {
 
-	@KafkaListener(topics = {"test"},groupId = "1212")
-	public void consumeMessage(ConsumerRecord<String, Student> payload) {
-		System.out.println("Kafka messages from test...." +payload.value() );
+	@KafkaListener(topics = { "test" }, groupId = "1212", filter = "filterEventsByMessage")
+	public void consumeMessage(ConsumerRecord<String, Student> payload, Acknowledgment ack) {
+		System.out.println("Kafka messages from test...." + payload.value());
+		ack.acknowledge();
 	}
-	
-	@KafkaListener(topics = {"partition-test"},groupId = "1212")
+
+	@KafkaListener(topics = { "partition-test" }, groupId = "1212")
 	public void consumeMessage1(ConsumerRecord<String, String> payload) {
-		System.out.println("Kafka messages from partition-test from group 1212...." +payload.value() );
+		System.out.println("Kafka messages from partition-test from group 1212...." + payload.value());
 	}
-	
-	@KafkaListener(topics = {"partition-test"},groupId = "1212")
+
+	@KafkaListener(topics = { "partition-test" }, groupId = "1212")
 	public void consumeMessage2(ConsumerRecord<String, String> payload) {
-		System.out.println("Kafka messages from partition-test from group 1212 from 2nd method...." +payload.value() );
+		System.out.println("Kafka messages from partition-test from group 1212 from 2nd method...." + payload.value());
 	}
 	/*
 	 * @KafkaListener(topics = {"partition-test"},groupId = "121234") public void
@@ -38,21 +41,25 @@ public class KafkaConsumer {
 	 * System.out.println("Kafka messages from partition-test from group 121234...."
 	 * +payload.value() ); }
 	 */
-	
-	@KafkaListener(topics = {"test1"},groupId = "1212", filter = "filterEventsByMessage")
+
+	@KafkaListener(topics = { "test1" }, groupId = "1212")
 	public void consumeMessage3(ConsumerRecord<String, String> payload) {
-		System.out.println("Kafka messages from test1...." +payload.value() );
+		System.out.println("Kafka messages from test1...." + payload.value());
 	}
-	
+
 	@Bean
 	public DefaultErrorHandler error() {
-		BackOff backOff=new FixedBackOff(1000L, 4L);
-	BiFunction<ConsumerRecord<?, ?>, Exception, BackOff> backOffFunction=(a,b)->backOff;
-		DefaultErrorHandler defaultErrorHandler=new KafkaCustomErrorHandler();
-	//defaultErrorHandler.setBackOffFunction(backOffFunction);
-		//defaultErrorHandler.addRetryableExceptions(ResourceNotFoundException.class);
-		//defaultErrorHandler.addNotRetryableExceptions(NullPointerException.class,ResourceAccessException.class);
-	//DefaultErrorHandler error=new DefaultErrorHandler(backOff)	;
-	return defaultErrorHandler;
+		// called when retries are exhausted from backoff
+		ConsumerRecordRecoverer recoverer = (c, e) -> {System.out.println("----------Inside Record Recoverer+:"+c.value());
+		
+		};// logs into database
+
+		//BackOff backOff = new FixedBackOff(1000L, 2L);
+		//BiFunction<ConsumerRecord<?, ?>, Exception, BackOff> backOffFunction = (a, b) -> backOff;
+		DefaultErrorHandler defaultErrorHandler = new KafkaCustomErrorHandler();
+		//DefaultErrorHandler defaultErrorHandler = new KafkaCustomErrorHandler(recoverer, backOff);
+		// defaultErrorHandler.addRetryableExceptions(ResourceNotFoundException.class);
+		// defaultErrorHandler.addNotRetryableExceptions(NullPointerException.class,ResourceAccessException.class);
+		return defaultErrorHandler;
 	}
 }
--- src/main/java/sl/ms/inventorymanagement/exception/KafkaCustomErrorHandler.java
+++ src/main/java/sl/ms/inventorymanagement/exception/KafkaCustomErrorHandler.java
@@ -8,6 +8,7 @@ import java.util.function.BiFunction;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.springframework.kafka.listener.ConsumerRecordRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.listener.MessageListenerContainer;
 import org.springframework.kafka.support.serializer.DeserializationException;
@@ -15,7 +16,19 @@ import org.springframework.util.backoff.BackOff;
 import org.springframework.util.backoff.FixedBackOff;
 
 public class KafkaCustomErrorHandler extends DefaultErrorHandler{
+	
+	public KafkaCustomErrorHandler(){
+		
+	}
+	
+	public KafkaCustomErrorHandler(ConsumerRecordRecoverer recoverer,BackOff backOff ){
+		super(recoverer,backOff);
+	}
 
+	public KafkaCustomErrorHandler(BackOff backOff ){
+		super(backOff);
+	}
+	
 	@Override
 	public void handleOtherException(Exception thrownException, Consumer<?, ?> consumer,
 			MessageListenerContainer container, boolean batchListener) {
@@ -30,15 +43,19 @@ public class KafkaCustomErrorHandler extends DefaultErrorHandler{
 	public void handleRemaining(Exception thrownException, List<ConsumerRecord<?, ?>> records, Consumer<?, ?> consumer,
 			MessageListenerContainer container) {
 		System.out.println("!!!!!!!!!Inside Remaining!!!!!!!!!!!!!!!");
-		BackOff backOff=new FixedBackOff(1000L, 3L);
+		BackOff backOff=new FixedBackOff(1000L, 1L);
 		BiFunction<ConsumerRecord<?, ?>, Exception, BackOff> backOffFunction=(a,b)->backOff;
 		setBackOffFunction(backOffFunction);
 		Map<Class<? extends Throwable>, Boolean> classifications=new HashMap<>();
 		classifications.put(DeserializationException.class, Boolean.TRUE);
 		
 		setClassifications(classifications, false);
-		setAckAfterHandle(false);
+		//setResetStateOnExceptionChange(true); //reset backoff state if current failure is different from previous failure
+		//setAckAfterHandle(false); //ack and commit offset when handle itself is not throwing any exception
+		//But always latest offset status(successor/failure) takes precedence even if previous has failures
+		//setCommitRecovered(true);
 		super.handleRemaining(thrownException, records, consumer, container);
+		//throw new ProductNotFound();
 	}
 	
 	@Override
